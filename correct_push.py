9d81c90814c030ce2f67fd733539484d68b1fb68
Anshuman Singh <anshumansingh0106@gmail.com>
2025-09-17 23:01:45 +0530

    Update correct_push.py

--- correct_push.py
+++ correct_push.py
@@ -1,11 +1,12 @@
 from machine import Pin
-from time import sleep
-led= Pin(2, Pin.OUT)
-push= Pin(18, Pin.IN)
+import time
+
+led = Pin(2, Pin.OUT)
+push = Pin(18, Pin.IN)
+
 while True:
-    if push.value():
-        led.on()
-        sleep(0.5)
-        led.off()
-        sleep(0.5)
-        print("working")
+    if push.value() == 1:  # Check if the button is pressed
+        led.value(1)       # Turn the LED on
+    else:
+        led.value(0)       # Turn the LED off
+    time.sleep(0.1)        # Add a small delay for stability
